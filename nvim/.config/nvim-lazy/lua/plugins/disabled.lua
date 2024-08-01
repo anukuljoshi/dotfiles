8642daaf41089c057bd4291213e32cad9cdd16d7
anukuljoshi <anukuljoshi1@gmail.com>
2024-08-02 00:55:58 +0530

    added lazy nvim config

---
+++ nvim/.config/nvim-lazy/lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+    --disable mini.pairs
+    { "akinsho/bufferline.nvim", enabled = false },
+    { "nvimdev/dashboard-nvim", enabled = false },
+    { "folke/flash.nvim", enabled = false },
+    { "rcarriga/nvim-notify", enabled = false },
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    { "folke/which-key.nvim", enabled = false },
+    { "folke/noice.nvim", enabled = false },
+    { "MunifTanjim/nui.nvim", enabled = false },
+}

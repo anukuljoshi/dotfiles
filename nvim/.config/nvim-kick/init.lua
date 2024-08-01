0f2e3a5fe9e57fa379a8f2f738e9d91b9e7ad269
anukuljoshi <anukuljoshi1@gmail.com>
2024-08-02 00:57:00 +0530

    nvim: formatter setup

--- nvim/.config/nvim-kick/init.lua
+++ nvim/.config/nvim-kick/init.lua
@@ -1,19 +1,19 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-require 'opts'
-require 'keymaps'
-require 'cmds'
+require("opts")
+require("keymaps")
+require("cmds")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
@@ -28,14 +28,14 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 
-require('lazy').setup('plugins', {})
+require("lazy").setup("plugins", {})
 
 -- Load the colorscheme here
 -- vim.cmd.colorscheme 'andromeda'
 -- vim.cmd.colorscheme 'tokyonight-night'
-vim.cmd.colorscheme 'tokyonight-storm'
+vim.cmd.colorscheme("tokyonight-storm")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+-- vim.opt.tabstop = 4
+-- vim.opt.shiftwidth = 4

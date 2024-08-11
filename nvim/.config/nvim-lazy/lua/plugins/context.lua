6fe275c271a42e8c81c88b3a2d3714ce166338b1
anukuljoshi <anukuljoshi1@gmail.com>
2024-08-11 14:26:42 +0530

    nvim-lazy: updated treesitter context config

--- nvim/.config/nvim-lazy/lua/plugins/context.lua
+++ nvim/.config/nvim-lazy/lua/plugins/context.lua
@@ -1,5 +1,21 @@
 return {
     "nvim-treesitter/nvim-treesitter-context",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    event = "BufRead",
+    event = "VeryLazy",
+    opts = function()
+        local tsc = require("treesitter-context")
+
+        LazyVim.toggle.map("<leader>ut", {
+            name = "Treesitter Context",
+            get = tsc.enabled,
+            set = function(state)
+                if state then
+                    tsc.enable()
+                else
+                    tsc.disable()
+                end
+            end,
+        })
+
+        return { mode = "cursor", max_lines = 3 }
+    end,
 }

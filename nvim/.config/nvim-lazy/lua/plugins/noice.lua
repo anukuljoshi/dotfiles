f0218804ce4531331379aea0a3b91a8e9dbaeb50
anukuljoshi <anukuljoshi1@gmail.com>
2024-08-11 16:15:03 +0530

    nvim-lazy: fixed noice position

--- nvim/.config/nvim-lazy/lua/plugins/noice.lua
+++ nvim/.config/nvim-lazy/lua/plugins/noice.lua
@@ -3,24 +3,47 @@ return {
     {
         "folke/noice.nvim",
         opts = function(_, opts)
-            opts.presets = {
-                command_palette = {
-                    views = {
-                        cmdline_popup = {
-                            position = {
-                                row = "40%",
-                                col = "50%",
-                            },
-                            size = {
-                                min_width = 80,
-                                width = "auto",
-                                height = "auto",
-                            },
-                        },
-                    },
+            opts.lsp.signature = {
+                auto_open = { enabled = false },
+                opts = {
+                    size = { max_height = 2 },
                 },
             }
         end,
+        config = function ()
+            require("noice").setup({
+                views = {
+                    cmdline_popup = {
+                        position = {
+                            row = "40%",
+                            col = "50%",
+                        },
+                        size = {
+                            width = 60,
+                            height = "auto",
+                        },
+                    },
+                    popupmenu = {
+                        relative = "editor",
+                        position = {
+                            row = "65%",
+                            col = "50%",
+                        },
+                        size = {
+                            width = 60,
+                            height = 10,
+                        },
+                        border = {
+                            style = "rounded",
+                            padding = { 0, 1 },
+                        },
+                        win_options = {
+                            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+                        },
+                    },
+                },
+            })
+        end
     },
 
 }

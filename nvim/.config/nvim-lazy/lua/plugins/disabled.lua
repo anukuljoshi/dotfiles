03d264f6d93d51b5b9a103730da6cb5a60c53593
anukuljoshi <anukuljoshi1@gmail.com>
2024-08-11 14:14:09 +0530

    nvim-lazy: updated disabled plugins list

--- nvim/.config/nvim-lazy/lua/plugins/disabled.lua
+++ nvim/.config/nvim-lazy/lua/plugins/disabled.lua
@@ -1,14 +1,13 @@
 return {
-    --disable mini.pairs
     { "akinsho/bufferline.nvim", enabled = false },
     { "nvimdev/dashboard-nvim", enabled = false },
     { "folke/flash.nvim", enabled = false },
     { "rcarriga/nvim-notify", enabled = false },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "folke/which-key.nvim", enabled = false },
-    { "folke/noice.nvim", enabled = false },
-    { "MunifTanjim/nui.nvim", enabled = false },
     { "echasnovski/mini.ai", enabled = false},
     { "echasnovski/mini.hipatterns", enabled = false},
     { "echasnovski/mini.pairs", enabled = false},
+    { "mfussenegger/nvim-lint", enabled = false},
+    { "folke/persistence.nvim", enabled = false},
 }

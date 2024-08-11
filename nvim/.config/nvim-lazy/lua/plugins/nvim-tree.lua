434e5ac0707addbd3b8fc99ebda08ba39c0d9182
anukuljoshi <anukuljoshi1@gmail.com>
2024-08-11 14:14:42 +0530

    nvim-lazy: minor changes

--- nvim/.config/nvim-lazy/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim-lazy/lua/plugins/nvim-tree.lua
@@ -4,9 +4,6 @@ return {
         "nvim-tree/nvim-tree.lua",
         version = "*",
         lazy = false,
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
         config = function()
             local api = require("nvim-tree.api")
 

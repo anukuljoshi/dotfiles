fbd9a262dfaefdb8e4e0762cbb9458cc19ec5e0c
anukuljoshi <anukul.joshi@geeksforgeeks.org>
2024-08-12 11:24:56 +0530

    nvim-lazy: updated lualine config

--- nvim/.config/nvim-lazy/lua/plugins/lualine.lua
+++ nvim/.config/nvim-lazy/lua/plugins/lualine.lua
@@ -1,23 +1,37 @@
+local function removeAll(T)
+    for k in pairs(T) do
+        T[k] = nil
+    end
+end
+
 return {
     {
         "nvim-lualine/lualine.nvim",
         event = "VeryLazy",
         opts = function(_, opts)
-
             -- remove diagnostics from lualine_c
             table.remove(opts.sections.lualine_c, 2)
             -- remove lsp symbols from lualine_c
             table.remove(opts.sections.lualine_c)
 
+            -- store diff from lualine_x in temp var
+            local lualine_diff = table.remove(opts.sections.lualine_x)
+
             -- remove all from lualine_x
-            opts.sections.lualine_x = {}
-            -- add git in lualine_x
-            table.insert(opts.sections.lualine_x, "diff")
-            -- add diagnostics in lualine_x
+            removeAll(opts.sections.lualine_x)
+
+            -- insert diagnostics in lualine_x
             table.insert(opts.sections.lualine_x, "diagnostics")
 
-            -- remove all from lualine_z
-            opts.sections.lualine_z = {}
+            -- remove values from lualine_y
+            removeAll(opts.sections.lualine_y)
+
+            -- insert diff info in lualine_y
+            table.insert(opts.sections.lualine_y, lualine_diff)
+
+            -- remove values from lualine_z
+            removeAll(opts.sections.lualine_z)
+            table.insert(opts.sections.lualine_z, { "location" })
         end,
     },
 }

f7963318572d1dab25f5371e9b612b9e5339b8ee
anukuljoshi <anukuljoshi1@gmail.com>
2024-08-29 00:58:30 +0530

    nvim: commented out noice plugin setup

--- nvim/.config/nvim-lazy/lua/plugins/noice.lua
+++ nvim/.config/nvim-lazy/lua/plugins/noice.lua
@@ -1,49 +1,51 @@
-return {
+return {}
 
-    {
-        "folke/noice.nvim",
-        opts = function(_, opts)
-            opts.lsp.signature = {
-                auto_open = { enabled = false },
-                opts = {
-                    size = { max_height = 2 },
-                },
-            }
-        end,
-        config = function ()
-            require("noice").setup({
-                views = {
-                    cmdline_popup = {
-                        position = {
-                            row = "40%",
-                            col = "50%",
-                        },
-                        size = {
-                            width = 60,
-                            height = "auto",
-                        },
-                    },
-                    popupmenu = {
-                        relative = "editor",
-                        position = {
-                            row = "65%",
-                            col = "50%",
-                        },
-                        size = {
-                            width = 60,
-                            height = 10,
-                        },
-                        border = {
-                            style = "rounded",
-                            padding = { 0, 1 },
-                        },
-                        win_options = {
-                            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-                        },
-                    },
-                },
-            })
-        end
-    },
-
-}
+-- return {
+--
+--     {
+--         "folke/noice.nvim",
+--         opts = function(_, opts)
+--             opts.lsp.signature = {
+--                 auto_open = { enabled = false },
+--                 opts = {
+--                     size = { max_height = 2 },
+--                 },
+--             }
+--         end,
+--         config = function ()
+--             require("noice").setup({
+--                 views = {
+--                     cmdline_popup = {
+--                         position = {
+--                             row = "40%",
+--                             col = "50%",
+--                         },
+--                         size = {
+--                             width = 60,
+--                             height = "auto",
+--                         },
+--                     },
+--                     popupmenu = {
+--                         relative = "editor",
+--                         position = {
+--                             row = "65%",
+--                             col = "50%",
+--                         },
+--                         size = {
+--                             width = 60,
+--                             height = 10,
+--                         },
+--                         border = {
+--                             style = "rounded",
+--                             padding = { 0, 1 },
+--                         },
+--                         win_options = {
+--                             winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+--                         },
+--                     },
+--                 },
+--             })
+--         end
+--     },
+--
+-- }

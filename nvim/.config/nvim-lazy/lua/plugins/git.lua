b59d198cb192d2c3748b872b00648b469c811231
anukuljoshi <anukul.joshi@geeksforgeeks.org>
2024-09-22 13:49:46 +0530

    nvim: added config for diffview plugin

--- nvim/.config/nvim-lazy/lua/plugins/git.lua
+++ nvim/.config/nvim-lazy/lua/plugins/git.lua
@@ -40,30 +40,211 @@ return {
     },
 
     {
+        "sindrets/diffview.nvim",
+        config = function()
+            -- Lua
+            local actions = require("diffview.actions")
+
+            require("diffview").setup({
+                view = {
+                    -- Configure the layout and behavior of different types of views.
+                    -- Available layouts:
+                    --  'diff1_plain'
+                    --    |'diff2_horizontal'
+                    --    |'diff2_vertical'
+                    --    |'diff3_horizontal'
+                    --    |'diff3_vertical'
+                    --    |'diff3_mixed'
+                    --    |'diff4_mixed'
+                    -- For more info, see |diffview-config-view.x.layout|.
+                    merge_tool = {
+                        -- Config for conflicted files in diff views during a merge or rebase.
+                        -- UPDATED
+                        layout = "diff3_mixed",
+                        disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
+                        winbar_info = true, -- See |diffview-config-view.x.winbar_info|
+                    },
+                },
+                file_panel = {
+                    win_config = { -- See |diffview-config-win_config|
+                        -- UPDATED
+                        position = "right",
+                        width = 35,
+                        win_opts = {},
+                    },
+                },
+                keymaps = {
+                    disable_defaults = false, -- Disable the default keymaps
+                    view = {
+                        -- The `view` bindings are active in the diff buffers, only when the current
+                        -- tabpage is a Diffview.
+                        -- UPDATED
+                        {
+                            "n", "]f", actions.select_next_entry, { desc = "Open the diff for the next file" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "[f", actions.select_prev_entry, { desc = "Open the diff for the previous file" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>gf", actions.goto_file_edit, { desc = "Open the file in the previous tabpage" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>gs", actions.goto_file_split, { desc = "Open the file in a new split" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>gt", actions.goto_file_tab, { desc = "Open the file in a new tabpage" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>b", actions.focus_files, { desc = "Bring focus to the file panel" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>e", function ()
+                                actions.toggle_files()
+                                vim.cmd("wincmd =")
+                            end, { desc = "Toggle the file panel." },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>gl", actions.cycle_layout, { desc = "Cycle through available layouts." },
+                        },
+                        {
+                            "n", "<leader>co", actions.conflict_choose("ours"), { desc = "Choose the OURS version of a conflict" },
+                        },
+                        {
+                            "n", "<leader>ct", actions.conflict_choose("theirs"), { desc = "Choose the THEIRS version of a conflict" },
+                        },
+                        {
+                            "n", "<leader>cb", actions.conflict_choose("base"), { desc = "Choose the BASE version of a conflict" },
+                        },
+                        {
+                            "n", "<leader>ca", actions.conflict_choose("all"), { desc = "Choose all the versions of a conflict" },
+                        },
+                        {
+                            "n", "<leader>cx", actions.conflict_choose("none"), { desc = "Delete the conflict region" },
+                        },
+                        {
+                            "n", "<leader>cO", actions.conflict_choose_all("ours"), { desc = "Choose the OURS version of a conflict for the whole file" },
+                        },
+                        {
+                            "n", "<leader>cT", actions.conflict_choose_all("theirs"), { desc = "Choose the THEIRS version of a conflict for the whole file" },
+                        },
+                        {
+                            "n", "<leader>cB", actions.conflict_choose_all("base"), { desc = "Choose the BASE version of a conflict for the whole file" },
+                        },
+                        {
+                            "n", "<leader>cA", actions.conflict_choose_all("all"), { desc = "Choose all the versions of a conflict for the whole file" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>cX", actions.conflict_choose_all("none"), { desc = "Delete the conflict region for the whole file" },
+                        },
+                    },
+                    file_panel = {
+                        {
+                            "n", "j", actions.select_next_entry, { desc = "Open the diff for the next file" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "k", actions.select_prev_entry, { desc = "Open the diff for the previous file" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>gf", actions.goto_file_edit, { desc = "Open the file in the previous tabpage" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>gs", actions.goto_file_split, { desc = "Open the file in a new split" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>gt", actions.goto_file_tab, { desc = "Open the file in a new tabpage" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>b", actions.focus_files, { desc = "Bring focus to the file panel" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>e", function ()
+                                actions.toggle_files()
+                                vim.cmd("wincmd =")
+                            end, { desc = "Toggle the file panel." },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>gl", actions.cycle_layout, { desc = "Cycle through available layouts." },
+                        },
+                        {
+                            "n", "<leader>co", actions.conflict_choose("ours"), { desc = "Choose the OURS version of a conflict" },
+                        },
+                        {
+                            "n", "<leader>ct", actions.conflict_choose("theirs"), { desc = "Choose the THEIRS version of a conflict" },
+                        },
+                        {
+                            "n", "<leader>cb", actions.conflict_choose("base"), { desc = "Choose the BASE version of a conflict" },
+                        },
+                        {
+                            "n", "<leader>ca", actions.conflict_choose("all"), { desc = "Choose all the versions of a conflict" },
+                        },
+                        {
+                            "n", "<leader>cx", actions.conflict_choose("none"), { desc = "Delete the conflict region" },
+                        },
+                        {
+                            "n", "<leader>cO", actions.conflict_choose_all("ours"), { desc = "Choose the OURS version of a conflict for the whole file" },
+                        },
+                        {
+                            "n", "<leader>cT", actions.conflict_choose_all("theirs"), { desc = "Choose the THEIRS version of a conflict for the whole file" },
+                        },
+                        {
+                            "n", "<leader>cB", actions.conflict_choose_all("base"), { desc = "Choose the BASE version of a conflict for the whole file" },
+                        },
+                        {
+                            "n", "<leader>cA", actions.conflict_choose_all("all"), { desc = "Choose all the versions of a conflict for the whole file" },
+                        },
+                        -- UPDATED
+                        {
+                            "n", "<leader>cX", actions.conflict_choose_all("none"), { desc = "Delete the conflict region for the whole file" },
+                        },
+
+                    }
+                },
+            })
+            vim.keymap.set("n", "<leader>dv", "<cmd>DiffviewOpen<cr>", { desc = "Open diffview" })
+            vim.keymap.set("n", "<leader>dx", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" })
+        end,
+    },
+
+    {
         -- git
         "lewis6991/gitsigns.nvim",
         opts = {},
         config = function()
             require("gitsigns").setup({
                 signs = {
-                    add          = { text = "┃" },
-                    change       = { text = "┃" },
-                    delete       = { text = "_" },
-                    topdelete    = { text = "‾" },
+                    add = { text = "┃" },
+                    change = { text = "┃" },
+                    delete = { text = "_" },
+                    topdelete = { text = "‾" },
                     changedelete = { text = "~" },
-                    untracked    = { text = "┆" },
+                    untracked = { text = "┆" },
                 },
                 signs_staged = {
-                    add          = { text = "┃" },
-                    change       = { text = "┃" },
-                    delete       = { text = "_" },
-                    topdelete    = { text = "‾" },
+                    add = { text = "┃" },
+                    change = { text = "┃" },
+                    delete = { text = "_" },
+                    topdelete = { text = "‾" },
                     changedelete = { text = "~" },
-                    untracked    = { text = "┆" },
+                    untracked = { text = "┆" },
                 },
                 signs_staged_enable = true,
-                signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-                numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+                signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+                numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
                 current_line_blame = false,
                 current_line_blame_opts = {
                     virt_text = true,

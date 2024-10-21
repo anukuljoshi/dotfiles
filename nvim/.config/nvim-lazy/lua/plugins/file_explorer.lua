ed8ac51f9a95b0379f751cd2765c442ee070d602
anukuljoshi <anukuljoshi1@gmail.com>
2024-10-21 22:05:59 +0530

    nvim-lazy: fixed neo-tree to open in cwd

--- nvim/.config/nvim-lazy/lua/plugins/file_explorer.lua
+++ nvim/.config/nvim-lazy/lua/plugins/file_explorer.lua
@@ -8,6 +8,24 @@ return {
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
         },
+        keys = {
+            {
+                "<leader>fe",
+                function()
+                    require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+                end,
+                desc = "Explorer NeoTree (cwd)",
+            },
+            {
+                "<leader>fE",
+                function()
+                    require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+                end,
+                desc = "Explorer NeoTree (Root Dir)",
+            },
+            { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+            { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
+        },
         config = {
             close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
             window = {

8de4d6b6e17b0bfc5a7a6821e58ef936dfb4b6e9
anukuljoshi <anukul.joshi@geeksforgeeks.org>
2024-11-17 14:21:28 +0530

    nvim-lazy: remove signature help

--- nvim/.config/nvim-lazy/lua/plugins/autocomplete.lua
+++ nvim/.config/nvim-lazy/lua/plugins/autocomplete.lua
@@ -1,8 +1,5 @@
 return {
     "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-    },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
         local has_words_before = function()
@@ -10,8 +7,6 @@ return {
             local line, col = unpack(vim.api.nvim_win_get_cursor(0))
             return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
         end
-
-        table.insert(opts.sources, { name = "nvim_lsp_signature_help" })
 
         local cmp = require("cmp")
 
